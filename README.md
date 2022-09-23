291dc89bf1b981940719c6cad0c719c237f0cca7
hecmendoza <hecmendoza20@hotmail.com>
2022-09-23 12:08:30 -0500

    Estructurando: Proyecto Base. HF

---
+++ README.md
@@ -0,0 +1,3 @@
+# git-course
+Curso de Youtube por Bye Code
+Gracias por ver
---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Bye Code!"
+
+@app.route('/hello')
+def greating():
+    return "Hello World!"
+
+@app.route('/sum/<int:a>/<int:b>')
+def sum(a: int, b: int):
+    nums_sum = a + b
+    return f"La suma es: {str(nums_sum)}"
+
+@app.route('/multiply/<int:a>/<int:b>')
+def multiply(a: int, b: int):
+    result = float(a * b)
+    return f"El resultado de la multiplicación es: {str(result)}"
